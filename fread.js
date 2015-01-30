73cef76b9de0fecf325912c8aa5765afc9465ffe
jawa9000 <jawa9000@gmail.com>
2015-01-30 08:31:47 -0800

    added error handling for opening/reading a file.

--- fread.js
+++ fread.js
@@ -3,17 +3,27 @@
 var fs = require('fs');
 
 // open file
-fs.open('test.txt','r', function(err,handle) {
-	console.log("reading test.txt now.");
-	var f = handle; // grab file
-	var b = new Buffer(100000); // set up buffer
-	
-	fs.read(f, b, 0, 100000, null, function(err, bytes_read) {
-		console.log(b.toString("utf8", 0, bytes_read)); // print file to console.log
-		fs.close(f); // close file
-	});
+var file = "test.txt";
+fs.open(file,'r', function(err,handle) {
+	if (err == null) { // if there's no error on opening the file
+		console.log("reading " + file + " now.");
+		var f = handle; // grab file
+		var b = new Buffer(100000); // set up buffer
+		
+		fs.read(f, b, 0, 100000, null, function(err, bytes_read) {
+			if (err == null) { // if there's no error on reading the file
+				console.log(b.toString("utf8", 0, bytes_read)); // print file to console.log
+			} else { // failed to read the file
+				console.log("failed on read: " + err.code + " " + err.message);
+			}
+			fs.close(f); // close file
+		});
+	} else { // failed to open the file
+		console.log("failed on open: " + err.code + " " + err.message);
+	}
 });
 
+/*
 var temp;
 fs.open('test.html','r', function(err,handle) {
 	console.log("reading test.html now.");
@@ -26,4 +36,4 @@ fs.open('test.html','r', function(err,handle) {
 		fs.close(f);
 	});
 });
-
+*/
